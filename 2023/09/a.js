83acd00d73584efd9a66483b0032048b2d695327
Jeremy Ma <jma@arrivelogistics.com>
2023-12-08 23:37:28 -0600

    2023 day 9

---
+++ 2023/09/a.js
@@ -0,0 +1,28 @@
+const fs = require('fs');
+
+const file = fs.readFileSync(`${__dirname}/input.txt`, { encoding: 'utf-8'});
+
+const lines = file.split('\r\n');
+
+let sum = 0;
+
+lines.forEach(line => {
+    const numberSeq = line.split(' ').map(num => parseInt(num));
+
+    const recurse = arr => {
+        if (arr.filter(num => num !== 0).length === 0) {
+            return 0
+        }
+
+        const diffArr = [];
+        for (let i = 1; i < arr.length; i++) {
+            diffArr.push(arr[i] - arr[i - 1]);
+        }
+        return recurse(diffArr) + arr[arr.length - 1];
+    }
+
+    const finalNum = recurse(numberSeq);
+    sum += finalNum;
+});
+
+console.log(sum);
